4b8ad4a075b904ddab871344269a913b30aae7f2
Matt Bierner <mattbierner@gmail.com>
2013-01-10 22:10:47 -0600

    Fixed formatting issued. Added check for end for map.

--- test/testMap.js
+++ test/testMap.js
@@ -1,5 +1,4 @@
 define(['stream'], function(stream){
-    
     return {
         'module': "Map Tests",
         'tests': [
@@ -11,6 +10,7 @@ define(['stream'], function(stream){
                 assert.equal(stream.first(g), 0);
                 assert.equal(stream.first(stream.rest(g)), 2);
                 assert.equal(stream.first(stream.rest(stream.rest(g))), 4);
+                assert.ok(stream.isEmpty(stream.rest(stream.rest(stream.rest(g)))));
             }],
         ],
     };
